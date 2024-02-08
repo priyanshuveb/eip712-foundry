1efe4bd1cdac3668f8626e8ff1f357b0f68c9f6c
priyanshuveb <priyanshubindal@gmail.com>
2024-02-09 03:06:50 +0530

    add natspec comments

--- src/eip2612/SigUtils.sol
+++ src/eip2612/SigUtils.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-
+///@title SigUtils
+///@author Priyanshu Bindal
+///@notice The contract helps us to calculate the digest which then can be signed by the user to
+/// generate the signature
 contract SigUtils {
     bytes32 public immutable DOMAIN_SEPARATOR;
     bytes32 public constant PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
+    ///@dev Initializes the DOMAIN_SEPERATOR
     constructor(bytes32 _DOMAIN_SEPARATOR) {
         DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
     }
@@ -20,16 +23,19 @@ contract SigUtils {
         uint256 deadline;
     }
 
-    // computes the hash of a permit
+    ///@notice Computes the hash of a permit struct
+    ///@param _permit The Permit struct having values about the signature
+    ///@return bytes32 Returns the struct hash that will be used to calculate digest
     function getStructHash(Permit memory _permit) public pure returns (bytes32) {
         return keccak256(
             abi.encode(PERMIT_TYPEHASH, _permit.owner, _permit.spender, _permit.value, _permit.nonce, _permit.deadline)
         );
     }
 
-    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
+    ///@notice Computes the hash of the fully encoded EIP-712 message with the domain, which can be used to recover the signer
+    ///@param _permit The Permit struct having values about the signature
+    ///@return bytes32 Returns the digest to be signed
     function getTypedDataHash(Permit memory _permit) public view returns (bytes32) {
         return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, getStructHash(_permit)));
-        // return MessageHashUtils.toTypedDataHash(DOMAIN_SEPARATOR, getStructHash(_permit));
     }
 }
