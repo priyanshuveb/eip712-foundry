ff4e33c76e91f9987168d6f68a7ee82db630c064
priyanshuveb <priyanshubindal@gmail.com>
2024-02-09 02:10:09 +0530

    deploy script remove hardcoded value

--- script/DeploySigUtils.s.sol
+++ script/DeploySigUtils.s.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {SigUtils} from "../src/eip2612/SigUtils.sol";
 
-contract DeployToken is Script {
+contract DeploySigUtils is Script {
     SigUtils sigUtils;
-    bytes32 domainSeperator = 0x24e7c66ed39001992c0df02b36350cadfcf3384a55a6aa83a382075e377468b6;
+    // bytes32 domainSeperator = 0x24e7c66ed39001992c0df02b36350cadfcf3384a55a6aa83a382075e377468b6;
 
-    function run() public returns (address) {
+    function run(bytes32 domainSeperator) public returns (address) {
         vm.broadcast();
         sigUtils = new SigUtils(domainSeperator);
 
